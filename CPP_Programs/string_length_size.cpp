b99058400aed5c0b9d5df72cacca4d23ee357b72
Raviraj <ravirajchilka10@gmail.com>
2023-09-21 22:41:57 -0400

    Create string_length_size.cpp

---
+++ CPP_Programs/string_length_size.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <array>
+#include <cstdint>
+
+int main() {
+    std::string my_string = std::string {"abcd"};
+    std::cout << my_string.size() << std::endl;
+    std::cout << my_string.length() << std::endl;
+    return 0;
+};
