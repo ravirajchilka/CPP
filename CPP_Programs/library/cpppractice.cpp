56f5aacaa7bd1230880c9c1c9ff390e060f8acca
Raviraj <ravirajchilka10@gmail.com>
2023-11-10 18:54:16 -0500

    create own library example

---
+++ CPP_Programs/library/cpppractice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdint>
+#include "mylibrary.h"
+
+//using namespace lib;
+
+int main() {
+    mylib::increment_num(890);
+    return 0;
+}
+
+
+
---
+++ CPP_Programs/library/mylibrary.h
@@ -0,0 +1,11 @@
+#ifndef MYLIBRARY_H_
+#define MYLIBRARY_H_
+
+namespace mylib {
+    void increment_num(std::uint16_t num) {
+        std::cout<< "incremenetd number is" << ++num << std::endl;
+    }
+}
+
+#endif
+
