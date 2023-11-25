ffecccfc5c4155eddaf72fdb084013738b9717b5
Raviraj <ravirajchilka10@gmail.com>
2023-11-25 00:17:39 -0500

    Create string_view.cpp

---
+++ CPP_Programs/string_view.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdint>
+#include <string>
+#include <string_view>
+
+void print_string(std::string_view st) {
+    std::cout << st; 
+    /*
+        with string_view we dont construct string in the function so heap 
+        memory allocation is avoided
+    */ 
+}
+
+int main() {
+    auto str = std::string {"this is string, a large string!!!"};
+    print_string(str);
+    return 0;
+};
