ac223dbef637bc44ef157fde737002f14dad341d
Raviraj <ravirajchilka10@gmail.com>
2023-11-24 22:37:05 -0500

    added comments

---
+++ CPP_Programs/boolalpha_and_npos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdint>
+#include <string>
+
+#include "dtypes.h"
+
+int main() {
+    std::string name = "john";
+    const auto indx1 = name.find("j",0);
+    std::cout << std::boolalpha << (indx1 == std::string::npos);
+    /*npos shows highest value of size of string if value need to be find doesnt exist in the strin then indx1 set to hight possible size of string
+    and can be obtain by std::string::npos and boolalpha basically converting it to boolean value*/
+    return 0;
+};
