ad105c62b3330798684de31444830b3c8ecdac57
Raviraj <ravirajchilka10@gmail.com>
2024-08-23 00:12:26 -0400

    Update assigment_operator_overloading.cpp

--- Concept_Programs/OOP/assigment_operator_overloading.cpp
+++ Concept_Programs/OOP/assigment_operator_overloading.cpp
@@ -4,29 +4,46 @@
 
 class Complex {
 	private:
-	uint16_t real, img;	
+	    uint16_t real, img;	
 
 	public: 
 	    Complex(std::uint16_t r, std::uint16_t i) {
-            real = i;
+            real = r;
             img = i;
 	    } 
 
-	void operator = (const Complex &C) {
-	    real = C.real;
-	    img = C.img;
-	}    
+        void operator = (const Complex &C) {
+            real = C.real;
+            img = C.img;
+        }    
 
-	void print() {
-	    std::cout<<real<<"+i"<<img<<std::endl;
-	}
+        void print() {
+            std::cout<<real<<"+i"<<img<<std::endl;
+        }
 };
 
 int main() {
-    Complex C1(5,8) , C2(4,6);
-    std::cout << "C1 complex number";    
+    // overloading assigment oeprator	
+    Complex C1(5,8), C2(4,6);
+    
+    std::cout << "Before overloading assignment operator" << std::endl;
+    std::cout << "C1 complex number ";    
     C1.print();	
+    
+    std::cout << "C2 complex number ";    
+    C2.print();
+
+    C1 = C2;
+    
+    std::cout << "After overloading assignment operator" << std::endl;
+    std::cout << "C1 complex number ";
+    C1.print();	
+    
+    std::cout << "C2 complex number ";    
+    C2.print();
+   
 	
     return 0;
 }
+
 
