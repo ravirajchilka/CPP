b5929a1016e431a22432667f8c6c83dcddd6a7a1
Raviraj <ravirajchilka10@gmail.com>
2024-09-26 23:57:29 -0400

    Update abstract_method.cpp

--- Concept_Programs/OOP/abstract_method.cpp
+++ Concept_Programs/OOP/abstract_method.cpp
@@ -1,41 +1,48 @@
 #include <iostream>
 
 class Shape {
-
   protected:
-    float dimension;
+      float dimension;
 
   public: 
-    void getDimensions() {
-      std::cin >> dimension;
-    }
-
-    virtual float calculateArea() = 0;
-};
-
-
-class Circle : public Shape (
-    public:
-      float calculateArea() {
-        return 3.14 * dimension * dimension;
+      void getDimensions() {
+          std::cin >> dimension;
       }
+
+      virtual float calculateArea() = 0;
 };
 
 
-class Square : public Shape (
-    public:
+class Circle : public Shape {
+  public:
       float calculateArea() {
-        return dimension * dimension;
+          return 3.14 * dimension * dimension;
+      }
+  };
+
+  class Square : public Shape {
+  public:
+      float calculateArea() {
+          return dimension * dimension;
       }
 };
 
 
 int main() {
+    Circle circle;
+    Square square;
 
-  return 0;
+    std::cout << "Enter the radius of the circle: " << std::endl;
+    circle.getDimensions();
+    
+    std::cout << "Area of circle is: " << circle.calculateArea() << std::endl;
 
+    std::cout << "Enter the side length of the square: " << std::endl;
+    square.getDimensions();
+    
+    std::cout << "Area of square is: " << square.calculateArea() << std::endl;
+
+    return 0;
 }
-
-
 
 
