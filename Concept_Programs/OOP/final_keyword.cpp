39be5c45221e171ef680281413c7d8da458b592b
Raviraj <ravirajchilka10@gmail.com>
2024-09-28 23:48:32 -0400

    Create final_keyword.cpp

---
+++ Concept_Programs/OOP/final_keyword.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+class Parent final {
+
+  
+
+};
+
+class Child: public Parent { // a final class type can not be used as a base class for inheritance
+
+
+
+};
+
+
+
+
+int main() {
+
+    return 0;
+}
+
