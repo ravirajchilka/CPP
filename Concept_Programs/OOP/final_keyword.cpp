07109b03b8b02a5702ff018f5b57c74f6310cce2
Raviraj <ravirajchilka10@gmail.com>
2024-09-28 23:49:10 -0400

    Update final_keyword.cpp

--- Concept_Programs/OOP/final_keyword.cpp
+++ Concept_Programs/OOP/final_keyword.cpp
@@ -7,7 +7,7 @@ class Parent final {
 
 };
 
-class Child: public Parent { // a final class type can not be used as a base class for inheritance
+class Child: public Parent { // a final class or struct type can not be used as a base class for inheritance
 
 
 
