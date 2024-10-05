69bb09d8d174216b83afb2f48cae1b8686224e6a
Raviraj <ravirajchilka10@gmail.com>
2024-10-04 23:56:39 -0400

    Create format.cpp

---
+++ Concept_Programs/Exception Handlings/format.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+int main() {
+
+  try {
+    int age = 15;
+    if(age>=18) {
+      std::cout << "Your eligibility to apply driving license" << std::endl;
+    } else {
+      throw 505;    
+    }
+  }
+
+  catch(int e) {
+    std::cout << "You must be at least 18 to apply for the license" << std::endl;
+    std::cout << "Error number: " << e << std::endl;
+  }
+
+  return 0;
+}
+
