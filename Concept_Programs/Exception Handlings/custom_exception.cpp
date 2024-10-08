9e43d79d3659d5b375dcabf88a21144d89e28716
Raviraj <ravirajchilka10@gmail.com>
2024-10-08 00:00:28 -0400

    Update custom_exception.cpp

--- Concept_Programs/Exception Handlings/custom_exception.cpp
+++ Concept_Programs/Exception Handlings/custom_exception.cpp
@@ -1,28 +1,28 @@
-#include <exception>
 #include <iostream>
+#include <exception>
+#include <string>
 
 class MyCustomException : public std::exception 
 {
-    public:
-        const char* what() {
-            std::cout << "My custom C++ exception" << std::endl;
-            return 0;
-        }
+  private:
+      std::string message;
 
+  public:
+      MyCustomException(const std::string& msg = "Custom C++ Exception") : message(msg) {}
+
+      const char* what() const noexcept override {
+          return message.c_str();
+      }
 };
 
 
 int main() 
-{
+{    
     try {
-      throw MyCustomException();
+        throw MyCustomException("This is a custom exception");
+    } catch (const MyCustomException& mce) {
+        std::cout << "Caught MyCustomException: " << mce.what() << std::endl;
     }
-    catch(const MyCustomException& mce) {
-      std::cout << "Catch mycstom exception" << std::endl;
-      std::cout << mce.what();
-    }
-
-
     return 0;
 }
 
