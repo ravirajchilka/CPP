20b5f9ea2cdded8a258ba59294d4541907f691d6
Raviraj <ravirajchilka10@gmail.com>
2025-02-15 20:19:46 -0500

    Fixed span container code

--- Concept_Programs/method_overloading_span.cpp
+++ Concept_Programs/method_overloading_span.cpp
@@ -4,19 +4,18 @@
 #include <vector>
 #include <span>
 
-void print_container_vals(std::span<std::uint16_t> span) {
-  for (const size_t val : ar) {
+void print_container_vals(std::span<const std::uint16_t> span) {
+  for (const std::uint16_t val : span) {
         std::cout << "from span array " << val << std::endl;
-    };
+    }
 }
 
 int main() {
-
-    std::vector<std::uint16_t> my_vec = std::vector<std::uint16_t>{1, 23, 3, 4, 5};
-    std::array<std::size_t, 5U> my_arr = std::array<std::size_t, 5U>{150, 2, 3, 4, 5};
+    std::vector<std::uint16_t> my_vec = {1, 23, 3, 4, 5};
+    std::array<std::uint16_t, 5U> my_arr = {150, 2, 3, 4, 5};
 
     print_container_vals(my_arr);
     print_container_vals(my_vec);
 
     return 0;
-};
+}
